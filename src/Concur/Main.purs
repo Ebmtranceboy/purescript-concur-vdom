fde32d99b9ac2023996087e9d042e2f572598535
Ebmtranceboy <ebmtranceboy@gmail.com>
2020-06-07 14:12:34 +0200

    remove Main

--- src/Concur/Main.purs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Concur.VDom (runWidgetInDom)
-import Concur.VDom.Widgets (sample)
-import Data.Unit (Unit)
-import Effect (Effect)
-
-main :: Effect Unit
-main = runWidgetInDom "root" sample
